cccd31687343efea9c23c6200eaff79dcb9fee03
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-03-24 13:40:38 -0400

    .

---
+++ vite-project/vite-project/javascript/domselectors.js
@@ -0,0 +1,11 @@
+let domselector = {
+    container: document.querySelector(".container"),
+    all_qwerty: document.querySelector("#all-qwerty"),
+    poster: document.querySelector("#poster"),
+    banner: document.querySelector("#banner"),
+    figurine: document.querySelector("#figurine"),
+    discount: document.querySelector("#discount"),
+    click: document.querySelector("theme"),
+};
+
+export { domselector };
