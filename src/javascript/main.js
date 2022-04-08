2c261569e3340ee4919c8459245fe44b72729c89
wheresiris <iris.lee22@sitechhs.com>
2022-04-08 13:19:30 -0400

    4/7

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,17 +1,17 @@
 import "../style.css";
-import { discount, qwerty } from "./array";
+import { qwerty } from "./array";
 import { domselector } from "./domselectors";
 
 function clear() {
-    domselector.container.innerHTML = "";
+  domselector.container.innerHTML = "";
 }
 //make all cards
 function create() {
-    qwerty.forEach((element) => {
-        function addcard() {
-            domselector.container.insertAdjacentHTML(
-                "beforeend",
-                `<div class = "item">
+  qwerty.forEach((element) => {
+    function addcard() {
+      domselector.container.insertAdjacentHTML(
+        "beforeend",
+        `<div class = "item">
           <div class = "text">
             <h2>${element.name}</h2>
             <h3>price: ${element.price}</h3>
@@ -20,22 +20,22 @@ function create() {
             </div>
           </div>
         </div>`
-            );
-        }
-        addcard();
-    });
+      );
+    }
+    addcard();
+  });
 }
 create();
 
 //all qwerty button filter
 domselector.all_qwerty.addEventListener("click", function (e) {
-    clear();
-    function specific() {
-        qwerty.forEach((element) => {
-            function addcard() {
-                domselector.container.insertAdjacentHTML(
-                    "beforeend",
-                    `<div class = "item">
+  clear();
+  function specific() {
+    qwerty.forEach((element) => {
+      function addcard() {
+        domselector.container.insertAdjacentHTML(
+          "beforeend",
+          `<div class = "item">
             <div class = "text">
               <h2>${element.name}</h2>
               <h3>price: ${element.price}</h3>
@@ -44,22 +44,21 @@ domselector.all_qwerty.addEventListener("click", function (e) {
               </div>
             </div>
           </div>`
-                );
-            }
-            addcard();
-        });
-    }
-    specific();
+        );
+      }
+      addcard();
+    });
+  }
+  specific();
 });
 
 //theme filter
 domselector.click.addEventListener("click", function () {
-    if (document.body.classList.contains("dark")) {
-        document.body.classList.add("light");
-        document.body.classList.remove("dark");
-    } else {
-        document.body.classList.add("dark");
-        document.body.classList.remove("light");
-    }
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  }
 });
-
