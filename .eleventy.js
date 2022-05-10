72c544d7b285054d7c5a9819a53059a72d1ac354
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-05-10 13:56:41 -0400

    a bit of change theme?

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/md");
     eleventyConfig.addPassthroughCopy("src/media");
     eleventyConfig.addPassthroughCopy("src/admin");
+    eleventyConfig.addPassthroughCopy("src/javascript");
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
