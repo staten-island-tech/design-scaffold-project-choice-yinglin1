444dc30ffbcf7d4ed7018d8a508e9181d1736268
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-05-24 13:58:35 -0400

    .

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -33,4 +33,27 @@ function toggleTheme() {
 
 
  */
+function SwitchTheme() {
 
+  var element = document.querySelector("body");
+  element.classList.toggle("dark-mode");
+
+  var elementB = document.querySelector("header");
+  elementB.classList.toggle("nav-dark-mode");
+
+
+  var x = document.getElementById("Sun");
+  var y = document.getElementById("Moon");
+
+     if (x.style.display === "none") {
+       x.style.display = "block";
+       y.style.display = "none";
+     } 
+
+     else {
+       x.style.display = "none";
+       y.style.display = "block";
+     }
+
+     console.log("VarSampleVal")
+}
