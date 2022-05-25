2236ae4d353b8943b5eac1df74b55fd736977311
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-05-24 20:37:32 -0400

    theme

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,4 @@
-import "../style.css";
+/* import "../style.css"; */
 
 /* const setTheme = theme => document.documentElement.className = theme;
 
@@ -33,3 +33,13 @@ function toggleTheme() {
 
 
  */
+document.querySelector(".btn").addEventListener("click", function() {
+  if(document.body.classList.contains("light")){
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  }else{
+    document.body.classList.add("light");
+    document.body.classList.remove("dark")
+  }
+}
+)
