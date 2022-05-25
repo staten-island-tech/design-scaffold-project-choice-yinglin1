e9baffa5588790d4bd8a3eb4aa162b73cbb2ec02
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-05-25 08:55:38 -0400

    .

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,45 +1,9 @@
-/* import "../style.css"; */
-
-/* const setTheme = theme => document.documentElement.className = theme;
-
-function setTheme(themeName) {
-  localStorage.setItem('theme', themeName);
-
-  document.documentElement.className = themeName;
-}
-
-//toggle between light and dark
-function toggleTheme() {
-  if (localStorage.getItem('theme') === 'dark') {
-    setTheme('light');
-  }
-  else {
-    setTheme('dark');
-  }
-}
-
-//invoke function on initial load
-(function () {
-  if (localStorage.getItem('theme') === 'dark') {
-    setTheme('dark');
-
-    document.getElementById('slider').checked = false;
-  } else {
-    setTheme('light');
-
-    document.getElementById('slider').checked = true;
-  }
-})();
-
-
- */
-document.querySelector(".btn").addEventListener("click", function() {
-  if(document.body.classList.contains("light")){
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
     document.body.classList.add("dark");
     document.body.classList.remove("light");
-  }else{
+  } else {
     document.body.classList.add("light");
-    document.body.classList.remove("dark")
+    document.body.classList.remove("dark");
   }
-}
-)
+});
